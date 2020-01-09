cf179fa7defb140605906df772d77d55dee2778a
한경만 <rudaks94@gmail.com>
2020-01-09 15:02:41 +0900

    sonarqube checking

--- attic_sonarqube_품질체크/js/background.js
+++ attic_sonarqube_품질체크/js/background.js
@@ -103,18 +103,20 @@ function getStorageSync(key) {
 	});
 }
 
+const SONARQUBE_URL = 'http://211.63.24.41:9000';
 
 let hasError = false;
 let saveStorageSync = {};
 
-const SONARQUBE_URL = 'http://211.63.24.41:9000/api/measures/search_history?component=spectra.attic%3Aplatform&metrics=bugs%2Cvulnerabilities%2Csqale_index%2Cduplicated_lines_density%2Cncloc%2Ccoverage%2Ccode_smells&ps=1000';
+// http://211.63.24.41:9000/api/measures/component?additionalFields=metrics%2Cperiods&component=spectra.attic%3Aplatform&metricKeys=alert_status%2Cquality_gate_details%2Cbugs%2Cnew_bugs%2Creliability_rating%2Cnew_reliability_rating%2Cvulnerabilities%2Cnew_vulnerabilities%2Csecurity_rating%2Cnew_security_rating%2Csecurity_hotspots%2Cnew_security_hotspots%2Ccode_smells%2Cnew_code_smells%2Csqale_rating%2Cnew_maintainability_rating%2Csqale_index%2Cnew_technical_debt%2Ccoverage%2Cnew_coverage%2Cnew_lines_to_cover%2Ctests%2Cduplicated_lines_density%2Cnew_duplicated_lines_density%2Cduplicated_blocks%2Cncloc%2Cncloc_language_distribution%2Cprojects%2Cnew_lines
+const SONARQUBE_CHECK_URL = `${SONARQUBE_URL}/api/measures/search_history?component=spectra.attic%3Aplatform&metrics=bugs%2Cvulnerabilities%2Csqale_index%2Cduplicated_lines_density%2Cncloc%2Ccoverage%2Ccode_smells&ps=1000`;
 
 function check()
 {
 	hasError = false;
 
 	const promises = [
-		$.ajax(SONARQUBE_URL)
+		$.ajax(SONARQUBE_CHECK_URL)
 	];
 	  
 	$.when.apply($, promises).then(
