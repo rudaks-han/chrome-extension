9917a7eb99c1b6faa08ca271fbc99b3687113eea
rudaks <rudaks94@gmail.com>
2020-03-05 00:42:17 +0900

    commit

--- mask/background.js
+++ mask/background.js
@@ -27,13 +27,98 @@ setTimeout(function() {
 			console.log(JSON.stringify(res))
 		}
 	});
-
-	//var params = 'device_iden=uju8k2VOqrYsjz7O3P0Jl6&email=rudaks94@gmail.com&channel_tag=&client_iden=&type=note&title=test&body=testbody';
-
-
 }, 1000);
 
+//var interval = 60*1000; // The display interval, in minutes.
 var interval = 60*1000; // The display interval, in minutes.
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+}
+
+function getDateFormat(date) {
+    return date.getFullYear() + '/' + makeTwoLength(date.getMonth()+1) + '/' + makeTwoLength(date.getDate()) + ' ' + makeTwoLength(date.getHours()) + ':' + makeTwoLength(date.getMinutes());
+}
+
+function makeTwoLength(str) {
+    if (String(str).length == 1) {
+        return '0' + str;
+    }
+    return str;
+}
+
+var beforeMinute = -1;
+var coronaMaskOpenDate = [];
+
+function checkCoronaMask() {
+    setTimeout(function() {
+        $.ajax({
+            type:"GET",
+            url:'https://coronamask.kr/',
+            success:function(res){
+                coronaMaskOpenDate.length = 0;
+
+                console.log(res);
+                var list = $(res).find('.relative.w-full.border-r');
+
+                list.each(function(index) {
+                    var url = $(this).find('a').attr('href');
+                    var text = $(this).find('.text-gray-600.leading-none.leading-normal').text();
+                    console.error('url: ' + url)
+                    console.error('text: ' + text)
+                    if (text.indexOf("시작") > -1) {
+                        console.error('text: ' + text);
+                        var date = text.substring(text.indexOf('시작')+4);
+                        var arDate = date.split(' ');
+                        var datePart = arDate[0].split('/'); // 2020/03/06
+                        var timePart = arDate[1].split(':'); // 10:00
+                        var year = datePart[0];
+                        var month = datePart[1];
+                        var day = datePart[2];
+                        var hour = timePart[0];
+                        var minute = timePart[1];
+
+                       /* console.error('year: ' + year);
+                        console.error('month: ' + month);
+                        console.error('day: ' + day);
+                        console.error('hour: ' + hour);
+                        console.error('minute: ' + minute);*/
+
+                        /*year = 2020
+                        month = 3
+                        day = 04
+                        hour = 23
+                        minute = 11*/
+                        var sellDate = new Date(year, Number(month)-1, day, hour, minute, 0);
+                        var newItem = {url:url, date: sellDate};
+
+                        //Array.prototype.push.apply(coronaMaskOpenDate, {url, sellDate});
+                        coronaMaskOpenDate.push(newItem)
+                        //var now = new Date();
+                        /*console.error('now: ' + sellDate);
+                        console.error('add 1minute: ' + addMinutes(sellDate, -1));
+                        console.error('now: ' + now);
+                        console.error('getDateFormat1: ' + getDateFormat(now));
+                        console.error('getDateFormat2: ' + getDateFormat(addMinutes(sellDate, beforeMinute)));*/
+
+
+                        /*if (getDateFormat(now) == getDateFormat(addMinutes(sellDate, beforeMinute))) {
+                            console.error('________________ 시작')
+                        } else {
+                            console.error('아직')
+                        }*/
+                    }
+                })
+            }
+        });
+    }, 100)
+}
+
+checkCoronaMask();
+
+setInterval(function() {
+
+}, 1000*60*5);
 
 function checkSite() {
 	welKipsMallCount = 0;
@@ -41,7 +126,7 @@ function checkSite() {
 	var name = '';
 	var url = '';
 
-	name = '웹킵스몰 스마트블랙 KF94 45개';
+	/*name = '웹킵스몰 스마트블랙 KF94 45개';
 	url = 'http://www.welkeepsmall.com/shop/shopdetail.html?branduid=1007193&xcode=023&mcode=002&scode=&type=X&sort=manual&cur_code=023&GfDT=bm95W1g%3D';
 	checkWelKipsMall(name, url);
 
@@ -83,7 +168,7 @@ function checkSite() {
 	checkNaverStore(name, url);
 
 	name = '네이버스토어 닥터퓨리 미세먼지지';
-	url = 'https://smartstore.naver.com/mfbshop/products/4072435942?NaPm=';
+	url = 'https://smartstore.naver.com/mfbshop/products/4072435942?site_preference=device&NaPm=';
 	checkNaverStore(name, url);
 
 	name = '네이버스토어 상공양행 마스크';
@@ -99,7 +184,7 @@ function checkSite() {
 	checkNaverStore(name, url);
 
 	name = '네이버스토어 공감이 뉴네퓨어 마스크';
-	url = 'https://smartstore.naver.com/gonggami/products/4705579501?NaPm=#QNA';
+	url = 'https://smartstore.naver.com/gonggami/products/4705579501';
 	checkNaverStore(name, url);
 
 	name = '네이버스토어 동국제약';
@@ -137,11 +222,11 @@ function checkSite() {
 	name = '네이버스토어 금아스토어 블랙';
 	url = 'https://smartstore.naver.com/kumaelectron/products/4813999869';
 	checkNaverStore(name, url);
-
+*/
     name = '네이버스토어 휴그린 중형';
     url = 'https://smartstore.naver.com/soommask/products/4828127993?NaPm=#DEFAULT';
     checkNaverStore(name, url);
-
+/*
     name = '네이버스토어 미마마스크';
     url = 'https://smartstore.naver.com/aseado/products/4837257765';
     checkNaverStore(name, url);
@@ -158,6 +243,14 @@ function checkSite() {
     url = 'https://smartstore.naver.com/carmang1825/products/4834056954';
     checkNaverStore(name, url);
 
+    name = '네이버스토어 라록스';
+    url = 'https://smartstore.naver.com/ygfac/products/3905641271';
+    checkNaverStore(name, url);
+    
+*/
+    name = '네이버스토어 마데카 파워 앰플';
+    url = 'https://smartstore.naver.com/dkpharm_naturesvitamin/products/4737857552';
+    checkNaverStore(name, url);
 
 
     /*
@@ -219,17 +312,56 @@ function checkWelKipsMall(name, url) {
 }
 
 function checkNaverStore(name, url) {
-	checkUrl(url, 'html', function(res) {
-		//if ($(res).find('.prd_type3').html().indexOf('구매하실 수 없는') == -1) {
-		//console.log(res.indexOf('<em class="fc_point">구매하실 수 없는</em> 상품입니다') == -1);
-		if (res.indexOf('배송비결제') > -1 && res.indexOf('<em class="fc_point">구매하실 수 없는</em> 상품입니다') == -1 && res.indexOf('현재 주문 폭주로 구매가 어렵습니다') == -1) {
-			pushBullet(name, url);
-			console.log('[판매중] ' + name + ' : ' + url);
-		} else {
-			console.log('[재고없음] ' + name);
-		}
-	});
+
+    console.error('url : ' + url)
+    var coronaMaskFound = false;
+    $.each(coronaMaskOpenDate, function(index, item) {
+        console.error(item.url)
+
+       //if (item.url == url) {
+            console.error('___있음 OOO')
+
+            // 시간체크
+            //openTab(url);
+            coronaMaskFound = true;
+            return false;
+       // }
+    });
+
+    if (coronaMaskFound) {
+        //openTab(url);
+        chrome.tabs.create({'url': url}, function(tab) {
+            console.error('Tab Created ' + tab.id);
+
+            chrome.tabs.executeScript(null, {file:'checkoutItem.js'});
+
+            //var code = 'alert(document.querySelector("._buy_button"))';
+            //chrome.tabs.executeScript(tab.id, {code: code});
+        });
+    }
+
+    /*checkUrl(url, 'html', function (res) {
+        if (res.indexOf('배송비결제') > -1 && res.indexOf('<em class="fc_point">구매하실 수 없는</em> 상품입니다') == -1 && res.indexOf('현재 주문 폭주로 구매가 어렵습니다') == -1) {
+            pushBullet(name, url);
+            console.log('[판매중] ' + name + ' : ' + url);
+        } else {
+            console.log('[재고없음] ' + name);
+        }
+    });*/
+
+
+};
+
+function openTab(url) {
+    chrome.tabs.create({ url: url });
+
+    checkSiteAvailable();
 }
+
+function checkSiteAvailable() {
+    
+}
+
 
 setInterval(function() {
 	console.log('checking... ' + new Date());
---
+++ mask/checkoutItem.js
@@ -0,0 +1,6 @@
+﻿var buyButton = document.querySelector("._buy_button");
+console.error('buyButton: ' + buyButton)
+
+if (buyButton != null) {
+	buyButton.click();
+}
