742b983ebf725edb3ce9a45c9602c175e853ffdf
rudaks <rudaks94@gmail.com>
2020-09-23 21:13:08 +0900

    ajax interceptor

---
+++ ajax_intercept/build.sh
@@ -0,0 +1,2 @@
+
+zip -r install.zip css/* images/* js/* manifest.json *.html *.js
---
+++ ajax_intercept/js/content-script.js
@@ -0,0 +1,14 @@
+﻿$( window ).ajaxComplete(function() {
+
+    console.error('complete')
+
+});
+
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('js/injected.js');
+s.onload = function() {
+    this.remove();
+};
+$(function() {
+    (document.head || document.documentElement).appendChild(s);
+})
